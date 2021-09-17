debe60ac8b2edea8bd49bd6f951e680330100605
Martin337 <gamarayburst@gmail.com>
2021-09-17 22:15:21 +0200

    Nadam se nema neki typo

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this code
+
+        var myNoun = "dog";
+        var myVerb = "ran";
+        var myAdjective = "big";
+        var myAdverb = "quickly";
+
+        var wordBlanks = "My " + myNoun + " always " + myVerb + " at " + myAdjective + " animals " + myAdverb + ".";
+
+    // Only change code above this code
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
